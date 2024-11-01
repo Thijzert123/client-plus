4811748314dc96beec7379bbc0fe829a29ca985d
Thijzert <184778919+Thijzert123@users.noreply.github.com>
2024-11-01 15:40:39 +0100

    Fix typo

--- packwiz/pack_content.py
+++ packwiz/pack_content.py
@@ -73,7 +73,7 @@
                            "extended-world-selection",
                            "clumps",
                            "legendary-tooltips",
-                           "flasback",
+                           "flashback",
                            "bettercommandblockui",
                            "improved-sign-editing",
                            "bindcommands"],
