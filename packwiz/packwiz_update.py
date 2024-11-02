b09f81873a7490a4b96795384f0c865570bbd1e3
Thijzert <184778919+Thijzert123@users.noreply.github.com>
2024-11-02 11:40:50 +0100

    Update pacwiz_update.py script

--- packwiz/packwiz_update.py
+++ packwiz/packwiz_update.py
@@ -21,30 +21,41 @@ def convert_to_abstract_modrinth_id(human_readable_id):
     project_details = r.json()
     return project_details["id"]
 
-def retrieve_existing_projects():
-    existing_projects = []
+def retrieve_pinned_projects():
+    pinned_projects = []
     for root, dirs, files in os.walk("."):
         for file_name in files:
             if file_name.endswith(PACKWIZ_SUFFIX):
                 file_path = os.path.join(root, file_name)
+                remove = True
+                abstract_modrinth_id = ""
                 with open(file_path, "rb") as file_content:
                     toml_data = tomllib.load(file_content)
                     abstract_modrinth_id = toml_data["update"]["modrinth"]["mod-id"]
-                    existing_projects.append(abstract_modrinth_id)
-    return existing_projects
+                    if "pin" in toml_data:
+                        if toml_data["pin"] == True:
+                            remove = False
+                if remove:
+                    print("Removing " + file_path)
+                    os.remove(file_path)
+                else:
+                    print(abstract_modrinth_id + " is pinned")
+                    pinned_projects.append(abstract_modrinth_id)
+    os.system("packwiz refresh")
+    return pinned_projects
 
 def main():
     if not os.path.isfile("pack.toml"):
         print("No pack.toml found, exiting")
         exit(0)
-    
+
     pack_content = eval(open("../pack_content.py").read())
-    existing_projects = retrieve_existing_projects()
+    pinned_projects = retrieve_pinned_projects()
 
     for content_type in pack_content:
         for project in pack_content[content_type]:
             abstract_modrinth_id = convert_to_abstract_modrinth_id(project)
-            if abstract_modrinth_id not in existing_projects:
+            if abstract_modrinth_id not in pinned_projects:
                 print("Adding project: " + project)
                 os.system("packwiz modrinth install --yes " + project)
             else:
